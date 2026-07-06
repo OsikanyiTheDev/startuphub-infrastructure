4e93a7b5ad6a8e4e0c2ddd83ddd839fc5a241621
OsikanyiTheDev <jillskillion@gmail.com>
2026-07-06 18:21:40 +0000

    ec2 module updated to compute

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -17,3 +17,15 @@ output "private_subnet_1_id" {
 output "private_subnet_2_id" {
   value = module.networking.private_subnet_2_id
 }
+
+output "instance_id" {
+  value = module.ec2.instance_id
+}
+
+output "public_ip" {
+  value = module.ec2.public_ip
+}
+
+output "public_dns" {
+  value = module.ec2.public_dns
+}
