4b08ab226ff3153062682976a166c8914d6835f0
OsikanyiTheDev <jillskillion@gmail.com>
2026-07-10 19:14:34 +0000

    testing

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,58 +1,57 @@
 resource "aws_lb" "this" {
-    name               = "${var.name}-alb"
-    internal           = false
-    load_balancer_type = "application"
-    
+  name               = "${var.name}-alb"
+  internal           = false
+  load_balancer_type = "application"
 
-    security_groups    = [var.alb_security_group_id]
 
-    subnets            = var.public_subnet_ids
+  security_groups = [var.alb_security_group_id]
 
-    enable_deletion_protection = var.enable_deletion_protection
+  subnets = var.public_subnet_ids
 
-    tags = {
-        Name = "${var.name}-alb"
-    }
+  enable_deletion_protection = var.enable_deletion_protection
+
+  tags = {
+    Name = "${var.name}-alb"
+  }
 }
 
 resource "aws_lb_target_group" "this" {
-    name     = "${var.name}-tg"
+  name = "${var.name}-tg"
 
-    port     = 80
+  port     = 3000
+  protocol = "HTTP"
+
+  vpc_id = var.vpc_id
+
+  target_type = "instance"
+
+  health_check {
+    path     = "/"
     protocol = "HTTP"
+    matcher  = "200"
 
-    vpc_id   = var.vpc_id
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 
-    target_type = "instance"
-
-    health_check {
-        path                = "/"
-        protocol            = "HTTP"
-        matcher             = "200"
-
-        interval            = 30
-        timeout             = 5
-
-        healthy_threshold   = 2
-        unhealthy_threshold = 2
-    }
-
-    tags = {
-        Name = "${var.name}-tg"
-    }
+  tags = {
+    Name = "${var.name}-tg"
+  }
 }
 
 resource "aws_lb_listener" "http" {
-    load_balancer_arn = aws_lb.this.arn
-    port              = 80
-    protocol          = "HTTP"
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
 
-    default_action {
-        type             = "forward"
-        target_group_arn = aws_lb_target_group.this.arn
-    }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
 
-    tags = {
-        Name = "${var.name}-listener"
-    }
+  tags = {
+    Name = "${var.name}-listener"
+  }
 }
--- modules/security/main.tf
+++ modules/security/main.tf
@@ -3,113 +3,113 @@
 #################################
 
 resource "aws_security_group" "alb" {
-    name = "${var.name}-alb-sg"
-    description = "Secuirty Group for the ALB"
-    vpc_id = var.vpc_id
+  name        = "${var.name}-alb-sg"
+  description = "Secuirty Group for the ALB"
+  vpc_id      = var.vpc_id
 
-    # HTTP access (environment controlled)
-    ingress {
-        description = "HTTP"
+  # HTTP access (environment controlled)
+  ingress {
+    description = "HTTP"
 
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = var.alb_http_cidr
-    }
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.alb_http_cidr
+  }
 
-    # HTTP acess (environment controlled)
-    ingress {
-        description = "HTTPS"
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = var.alb_https_cidr
-    }
+  # HTTP acess (environment controlled)
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.alb_https_cidr
+  }
 
-    egress {
-        description = "All outbound traffic"
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    description = "All outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "${var.name}-alb-sg"
-    }
+  tags = {
+    Name = "${var.name}-alb-sg"
+  }
 }
 
 #######################################
 #EC2 Security Group
 #######################################
 resource "aws_security_group" "ec2" {
-    name       = "${var.name}-ec2-sg"
-    description = "Allows application traffic from ALB only"
-    vpc_id     = var.vpc_id
+  name        = "${var.name}-ec2-sg"
+  description = "Allows application traffic from ALB only"
+  vpc_id      = var.vpc_id
 
 
-    #HTTP access ( from ALB)
-    ingress {
-        description = "HTTP from ALB"
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        security_groups = [
-            aws_security_group.alb.id
-        ]
-    }
+  #HTTP access ( from ALB)
+  ingress {
+    description = "HTTP from ALB"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    security_groups = [
+      aws_security_group.alb.id
+    ]
+  }
 
-    #outbound traffic
-    egress {
-        description = "All outbound traffic"
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  #outbound traffic
+  egress {
+    description = "All outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "${var.name}-ec2-sg"
-    }
+  tags = {
+    Name = "${var.name}-ec2-sg"
+  }
 }
 
 
 
 resource "aws_security_group" "rds" {
-    name = "${var.name}-rds-sg"
-    description = "Allos PostgreSQL acess only from the EC2 instances"
+  name        = "${var.name}-rds-sg"
+  description = "Allos PostgreSQL acess only from the EC2 instances"
 
-    vpc_id = var.vpc_id
+  vpc_id = var.vpc_id
 
-    ################################
-    # PostgreSQL from EC2 only
-    ################################
-    ingress {
-        description = "PostgreSQL from EC2"
-        from_port = 5432
-        to_port = 5432
-        protocol = "tcp"
-        security_groups = [
-            aws_security_group.ec2.id
-        ]
-    }
+  ################################
+  # PostgreSQL from EC2 only
+  ################################
+  ingress {
+    description = "PostgreSQL from EC2"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    security_groups = [
+      aws_security_group.ec2.id
+    ]
+  }
 
-    ################################
-    # Outbound
-    ################################
-    egress {
-        description = "All outbound traffic"
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = [
-            "0.0.0.0/0"
-        ]
-    }
+  ################################
+  # Outbound
+  ################################
+  egress {
+    description = "All outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
 
-    tags = {
-        Name = "${var.name}-rds-sg"
-    }
+  tags = {
+    Name = "${var.name}-rds-sg"
+  }
 
 
 }
